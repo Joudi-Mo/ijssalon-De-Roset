d1dddad0c22376cd4096442ae900e1556c88fa51
Joudi Mohamad <jr.mm65y@gmail.com>
2022-10-13 21:47:36 +0200

    product delete

--- views/medewerker/product_delete.php
+++ views/medewerker/product_delete.php
@@ -3,7 +3,7 @@ require "../../Classes/Database.php";
 
 $id = $_GET['id'];
 
-$sql = "DELETE FROM `products` WHERE product_id = $id";
+$sql = "DELETE FROM `products` WHERE id = $id";
 
 if (mysqli_query($conn, $sql)) {
     header("location: producten_overzicht.php");
